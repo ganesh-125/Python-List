6cc018aa35844ff7a4cdec5ca0a7c4d34485da2f
Ganesh Prajapati <ganeshprajapati@Ganeshs-MacBook-Pro.local>
2021-07-10 19:11:35 +0530

    List append vs extend

---
+++ list_append_vs_extend.py
@@ -0,0 +1,19 @@
+# Define a Empty list
+List1=[]
+# List with values
+List2=[1,2,3]
+# Appending the b into a
+List1.append(List2)
+print("Append :",List1)
+
+
+
+# Define a Empty list
+List1=[]
+# List with values
+List2=[1,2,3]
+
+
+# extend v/s append 
+List1.extend(List2)
+print("Extend :",List1)
